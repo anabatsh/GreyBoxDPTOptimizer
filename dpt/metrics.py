be39526bad23f97bdfde610dd8443b411eaafe6b
anabatsh <nbatsheva@gmail.com>
2025-03-03 17:10:55 +0300

    metrics fixed

---
+++ dpt/metrics.py
@@ -0,0 +1,22 @@
+import torch
+from torch import nn
+
+
+class Metrics(nn.Module):
+    def __init__(self):    
+        super().__init__()
+
+    def forward(self, targets, predictions, *args, **kwargs):
+        """
+        predictions - [batch_size, seq_len + 1, action_dim]
+        targets     - [batch_size, action_dim]
+        """
+        targets = targets.long()
+        accuracy = (predictions == targets[:, None]).float()
+        mae = torch.abs(predictions - targets[:, None]).float()
+        return {
+            "accuracy": accuracy.mean(), 
+            "accuracy_last": accuracy[:, -1].mean(),
+            "x_mae": mae.mean(),
+            "x_mae_last": mae[:, -1].mean(),
+        }
